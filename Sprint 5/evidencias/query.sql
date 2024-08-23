921e5309fcd7aca0e9c7750e187c0aca02bfc5f8
Artur da Silva <arturmariano004@gmail.com>
2024-08-23 17:11:46 -0300

    Conclusão da Sprint 5

--- Sprint 5/evidencias/query.sql
+++ Sprint 5/evidencias/query.sql
@@ -1,7 +1,6 @@
 SELECT 
     MAX(DATE_DIFF(month, TO_TIMESTAMP("Data do Inicio"), TO_TIMESTAMP("Data da Deflagracao"))) AS "Maior diferença deflagração e início (meses)",
-    SUM(CASE WHEN LOWER("Tipo de Operacao") = 'operacao especial' THEN 1 ELSE 0 END) AS "Total Operações Especiais",
-    COUNT(*) AS "Total de Registros"
+    SUM(CASE WHEN LOWER("Tipo de Operacao") = 'operacao especial' THEN 1 ELSE 0 END) AS "Total Operações Especiais"
 FROM 
     S3Object
 WHERE 
