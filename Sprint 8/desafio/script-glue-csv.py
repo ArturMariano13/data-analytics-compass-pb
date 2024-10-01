898033141af0123c9eeecc18a510d773f3e66fb9
Artur da Silva <arturmariano004@gmail.com>
2024-09-30 21:24:05 -0300

    Alterações 30/09

---
+++ Sprint 8/desafio/script-glue-csv.py
@@ -0,0 +1,37 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.sql import functions as F
+from pyspark.sql.types import IntegerType  # Adicionando o tipo numérico
+
+# @params: [JOB_NAME, S3_INPUT_PATH, S3_TARGET_PATH]
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'S3_INPUT_PATH', 'S3_TARGET_PATH'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Paths
+source_file = args['S3_INPUT_PATH']
+target_path = args['S3_TARGET_PATH']
+
+# 1. Leitura de arquivos locais
+df_dynamic = glueContext.create_dynamic_frame.from_options(
+    "s3",
+    {
+        "paths": [source_file]
+    },
+    "csv",
+    {"withHeader": True, "separator": "|"},
+)
+
+# Convertendo para um DataFrame Spark
+df_spark = df_dynamic.toDF()
+
+# 2. Imprimindo o schema original
+df_spark.printSchema()
